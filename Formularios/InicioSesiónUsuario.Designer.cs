77564867e8eec8aeb424b79dd14a8c1901a89d0c
JulioMndez <juliocesarmendez2905@gmail.com>
2024-11-14 21:26:30 -0600

    Agregue pantalla inicio de sesión Usuario

---
+++ Formularios/InicioSesiónUsuario.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ProyectoFinal.Formularios
+{
+    partial class InicioSesiónUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InicioSesiónUsuario));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblInicioSesionUsuario = new System.Windows.Forms.Label();
+            this.pbLogoUAM = new System.Windows.Forms.PictureBox();
+            this.lblIngresoDatosUsuario = new System.Windows.Forms.Label();
+            this.lblCIF = new System.Windows.Forms.Label();
+            this.pbCIF = new System.Windows.Forms.PictureBox();
+            this.txtCIF = new System.Windows.Forms.TextBox();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.pbContraseña = new System.Windows.Forms.PictureBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoUAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCIF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbContraseña)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(12, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 22);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.Location = new System.Drawing.Point(12, 428);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 22);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel3.Controls.Add(this.pbLogoUAM);
+            this.panel3.Controls.Add(this.lblInicioSesionUsuario);
+            this.panel3.Location = new System.Drawing.Point(1, 22);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(798, 85);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel4.Location = new System.Drawing.Point(1, 367);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(798, 61);
+            this.panel4.TabIndex = 2;
+            // 
+            // lblInicioSesionUsuario
+            // 
+            this.lblInicioSesionUsuario.AutoSize = true;
+            this.lblInicioSesionUsuario.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicioSesionUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblInicioSesionUsuario.Location = new System.Drawing.Point(11, 32);
+            this.lblInicioSesionUsuario.Name = "lblInicioSesionUsuario";
+            this.lblInicioSesionUsuario.Size = new System.Drawing.Size(462, 33);
+            this.lblInicioSesionUsuario.TabIndex = 0;
+            this.lblInicioSesionUsuario.Text = "UniEventHub - Inicio de Sesión Usuario\r\n";
+            // 
+            // pbLogoUAM
+            // 
+            this.pbLogoUAM.Image = ((System.Drawing.Image)(resources.GetObject("pbLogoUAM.Image")));
+            this.pbLogoUAM.Location = new System.Drawing.Point(627, 6);
+            this.pbLogoUAM.Name = "pbLogoUAM";
+            this.pbLogoUAM.Size = new System.Drawing.Size(138, 67);
+            this.pbLogoUAM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogoUAM.TabIndex = 1;
+            this.pbLogoUAM.TabStop = false;
+            // 
+            // lblIngresoDatosUsuario
+            // 
+            this.lblIngresoDatosUsuario.AutoSize = true;
+            this.lblIngresoDatosUsuario.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngresoDatosUsuario.Location = new System.Drawing.Point(34, 114);
+            this.lblIngresoDatosUsuario.Name = "lblIngresoDatosUsuario";
+            this.lblIngresoDatosUsuario.Size = new System.Drawing.Size(292, 22);
+            this.lblIngresoDatosUsuario.TabIndex = 3;
+            this.lblIngresoDatosUsuario.Text = "Por favor, Ingrese sus credenciales:";
+            // 
+            // lblCIF
+            // 
+            this.lblCIF.AutoSize = true;
+            this.lblCIF.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCIF.Location = new System.Drawing.Point(77, 156);
+            this.lblCIF.Name = "lblCIF";
+            this.lblCIF.Size = new System.Drawing.Size(40, 22);
+            this.lblCIF.TabIndex = 4;
+            this.lblCIF.Text = "CIF";
+            // 
+            // pbCIF
+            // 
+            this.pbCIF.Image = ((System.Drawing.Image)(resources.GetObject("pbCIF.Image")));
+            this.pbCIF.Location = new System.Drawing.Point(38, 151);
+            this.pbCIF.Name = "pbCIF";
+            this.pbCIF.Size = new System.Drawing.Size(33, 27);
+            this.pbCIF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCIF.TabIndex = 5;
+            this.pbCIF.TabStop = false;
+            // 
+            // txtCIF
+            // 
+            this.txtCIF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCIF.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCIF.Location = new System.Drawing.Point(38, 185);
+            this.txtCIF.Name = "txtCIF";
+            this.txtCIF.Size = new System.Drawing.Size(172, 26);
+            this.txtCIF.TabIndex = 6;
+            this.txtCIF.Text = "Ingrese su CIF";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.Location = new System.Drawing.Point(77, 226);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(99, 22);
+            this.lblContraseña.TabIndex = 7;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // pbContraseña
+            // 
+            this.pbContraseña.Image = ((System.Drawing.Image)(resources.GetObject("pbContraseña.Image")));
+            this.pbContraseña.Location = new System.Drawing.Point(38, 221);
+            this.pbContraseña.Name = "pbContraseña";
+            this.pbContraseña.Size = new System.Drawing.Size(33, 27);
+            this.pbContraseña.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbContraseña.TabIndex = 8;
+            this.pbContraseña.TabStop = false;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtContraseña.Location = new System.Drawing.Point(38, 254);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(172, 26);
+            this.txtContraseña.TabIndex = 9;
+            this.txtContraseña.Text = "Ingrese su Contraseña";
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnAcceder.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAcceder.Location = new System.Drawing.Point(38, 301);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(79, 30);
+            this.btnAcceder.TabIndex = 10;
+            this.btnAcceder.Text = "Acceder";
+            this.btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.Red;
+            this.btnRegresar.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegresar.Location = new System.Drawing.Point(135, 301);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(90, 30);
+            this.btnRegresar.TabIndex = 11;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            // 
+            // InicioSesiónUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.pbContraseña);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.txtCIF);
+            this.Controls.Add(this.pbCIF);
+            this.Controls.Add(this.lblCIF);
+            this.Controls.Add(this.lblIngresoDatosUsuario);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "InicioSesiónUsuario";
+            this.Text = "InicioSesiónUsuario";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoUAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCIF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbContraseña)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblInicioSesionUsuario;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pbLogoUAM;
+        private System.Windows.Forms.Label lblIngresoDatosUsuario;
+        private System.Windows.Forms.Label lblCIF;
+        private System.Windows.Forms.PictureBox pbCIF;
+        private System.Windows.Forms.TextBox txtCIF;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.PictureBox pbContraseña;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
